8ef879f67685bc1f4406839e65497f29024d72ad
Jiten Vij <jiten_vij@epam.com>
2019-03-01 19:46:19 +0530

    Added liquibase configurations

--- src/main/java/com/jpop/productservice/controller/ProductServiceController.java
+++ src/main/java/com/jpop/productservice/controller/ProductServiceController.java
@@ -6,15 +6,20 @@ import com.jpop.productservice.model.dto.ProductDTO;
 import com.jpop.productservice.service.ProductDeleteService;
 import com.jpop.productservice.service.ProductDetailService;
 import com.jpop.productservice.service.ProductInsertService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,36 +38,74 @@ public class ProductServiceController {
         this.productDeleteService = productDeleteService;
     }
 
-    @GetMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get all products", notes = "This will be used to get all the products in inventory")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Product found in inventory"),
+            @ApiResponse(code = 404, message = "URL not found"),
+            @ApiResponse(code = 400, message = "Bad request")
+    })
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Product>> getAllProducts() {
         return ResponseEntity.ok(productDetailService.getAvailableProducts());
     }
 
+    @ApiOperation(value = "Insert new products", notes = "This will be used to add products in inventory")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Product added in inventory"),
+            @ApiResponse(code = 404, message = "URL not found"),
+            @ApiResponse(code = 400, message = "Bad request")
+    })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> insertProduct(@RequestBody ProductDTO payload) {
+    public ResponseEntity<Product> insertProduct(@RequestBody ProductDTO payload) {
 
         logger.info("Payload received to insert data in product service with value {}", payload);
         ModelMapper modelMapper = new ModelMapper();
         Product productMap = modelMapper.map(payload, Product.class);
-        productInsertService.processRawData(productMap);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Successfully added product in repository");
+        Product savedProduct = productInsertService.processRawData(productMap);
+        final URI uri =
+                MvcUriComponentsBuilder.fromController(getClass())
+                        .path("/{id}")
+                        .buildAndExpand(savedProduct.getId())
+                        .toUri();
+        return ResponseEntity.created(uri).body(savedProduct);
     }
 
-    @GetMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> getProductDetails(@PathVariable long id) {
+    @ApiOperation(value = "Find product by ID", notes = "This will be used to find the product by using product ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Product found in inventory"),
+            @ApiResponse(code = 204, message = "Invalid ID supplied"),
+            @ApiResponse(code = 404, message = "URL not found"),
+            @ApiResponse(code = 400, message = "Bad request")
+    })
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getProductDetails(@ApiParam(value = "Numeric Product ID that needs to be find", required = true)@PathVariable long id) {
 
         logger.info("Received request to get details of product having id {}", id);
         String productData = new Gson().toJson(productDetailService.getProductDetails(id));
         return ResponseEntity.ok(productData);
     }
 
-    @DeleteMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity deleteProductById(@PathVariable long id) {
+    @ApiOperation(value = "Delete product by ID", notes = "This will be used to delete the product by using product ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Product deleted from inventory"),
+            @ApiResponse(code = 204, message = "Invalid ID supplied"),
+            @ApiResponse(code = 404, message = "URL not found"),
+            @ApiResponse(code = 400, message = "Bad request")
+    })
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity deleteProductById(@ApiParam(value = "Numeric Product ID that need to be delete", required = true)@PathVariable long id) {
         logger.info("Request received to delete product having id {}", id);
         productDeleteService.deleteProduct(id);
         return ResponseEntity.ok("Successfully deleted data");
     }
 
+    @ApiOperation(value = "Update product by ID", notes = "This will be used to update the product by using product ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Product updated in inventory"),
+            @ApiResponse(code = 204, message = "Invalid ID supplied"),
+            @ApiResponse(code = 404, message = "URL not found"),
+            @ApiResponse(code = 400, message = "Bad request")
+    })
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity updateProduct(@RequestBody ProductDTO payload, @PathVariable long id) {
         logger.info("Received request to update the product having product id {} and payload {}", id, payload);
--- src/test/java/com/jpop/productservice/integration/ProductServiceApplicationIT.java
+++ src/test/java/com/jpop/productservice/integration/ProductServiceApplicationIT.java
@@ -1,16 +1,33 @@
 package com.jpop.productservice.integration;
 
+import com.google.gson.Gson;
 import com.jpop.productservice.ProductServiceApplication;
+import com.jpop.productservice.dao.ProductInsertRepository;
+import com.jpop.productservice.model.Product;
+import com.jpop.productservice.service.ProductDetailService;
+import com.jpop.productservice.service.ProductInsertService;
+import com.jpop.productservice.service.impl.ProductDetailServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
 @RunWith(SpringRunner.class)
@@ -23,6 +40,18 @@ public class ProductServiceApplicationIT {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @Autowired
+    ProductDetailService productDetailService;
+
+    @TestConfiguration
+    static class ProductServiceApplicationITConfig {
+
+        @Bean
+        public ProductDetailService productDetailService() {
+            return new ProductDetailServiceImpl();
+        }
+    }
+
     @Test
     public void testGetProducts() {
         HttpHeaders headers = new HttpHeaders();
@@ -30,11 +59,35 @@ public class ProductServiceApplicationIT {
         headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
         HttpEntity httpEntity = new HttpEntity(null, headers);
 
-        ResponseEntity<String> responseEntity = testRestTemplate.exchange(createUrl(), HttpMethod.GET, httpEntity, String.class);
+        ResponseEntity<String> responseEntity = testRestTemplate.exchange(createUrl("/api/v1/products/"), HttpMethod.GET, httpEntity, String.class);
         assertEquals("Status code is invalid", 200, responseEntity.getStatusCode().value());
     }
 
-    private String createUrl() {
-        return "http://localhost:" + port + "/api/v1/products/";
+    @Test
+    public void testGetProductByID() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity httpEntity = new HttpEntity(null, headers);
+
+        ResponseEntity<String> responseEntity = testRestTemplate.exchange(createUrl("/api/v1/products/1"), HttpMethod.GET, httpEntity, String.class);
+        assertEquals("Status code is invalid", 200, responseEntity.getStatusCode().value());
+        assertNotNull("Data is null for the product ID", responseEntity.getBody());
+    }
+
+    @Test
+    public void testInsertProduct() {
+        Product product = new Product(3, "Shirt", "Adidas", new BigDecimal(123.98));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity httpEntity = new HttpEntity(new Gson().toJson(product), headers);
+        ResponseEntity<Product> responseEntity = testRestTemplate.exchange(createUrl("/api/v1/products/"), HttpMethod.POST, httpEntity, Product.class);
+        assertEquals("Status code is invalid", 201, responseEntity.getStatusCode().value());
+        assertEquals("Name is not correct",product.getName(), responseEntity.getBody().getName());
+    }
+
+    private String createUrl(String path) {
+        return "http://localhost:" + port + path;
     }
 }
